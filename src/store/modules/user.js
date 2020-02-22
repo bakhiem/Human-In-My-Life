990f7f7243334de09762843d7cd2c2028923a282
bakhiem <khiemnb2705@gmail.com>
2020-02-22 17:05:28 +0700

    update package.json

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,11 +1,7 @@
-
-
-
 const user = {
     state: {
       info: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : {},
       token: localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token')) : {},
     },
 }
-
 export default user
